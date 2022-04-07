6a569d1eaf7ff2c74c1ebdac7bb20860983365e1
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-04-06 22:25:09 -0500

    create stucture in project with express--create
    construyendi las bases y rutas basicas del sercer node

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const {main}=require('./helpers/pupee.js');
+//const {main}=require('./helpers/pupee.js');
 const express=require('express');
-const app=express();
+const path=require('path');
+const logger=require('morgan');
+const cookieParser = require('cookie-parser');
 const PORT=3000;
+//creamos instancia de express
+const app=express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello world');
+//settings
+app.set('port',process.env.PORT || PORT);
+//middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser());
+
+//static files
+const options = {
+    dotfiles: 'ignore',
+    etag: true,
+    extensions: ['php','html', 'htm'],
+    fallthrough:true,
+    inmutable:false,
+    index: 'index.html',
+    lastModified:true,
+    maxAge: '1d',
+    redirect: true,
+    setHeaders: (res, path, stat)=>res.set('x-timestamp', Date.now())
+  }
+//app.use(express.static(path.join(__dirname,'public'),options));
+
+//rutas
+app.use('/',express.static(path.join(__dirname,'public'),options))
+app.use('/home',require('./routes/home.js'));
+app.use('/users',require('./routes/users.js'));
+
+//Rutas indefinidas
+app.use((req,res,next)=>{
+    res.status(404).send('Direccion no existente en este enrutador')
 })
-app.get('/puppeteer/',(req,res)=>{
-    res.send('Inicializo el servicio de web scrapping');
-})
-app.listen(PORT,()=>{
+app.listen(app.get('port'),()=>{
     console.log('execute in port 3000')
 })
