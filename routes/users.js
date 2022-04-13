9a965c11cd16089adf4bb21f509a5441f1fa6732
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-04-13 01:29:52 -0500

    asignando controllers y path resolve para ruta inicial

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 let router = require('express').Router();
 let bodyParser=require('body-parser');
+let path=require('path');
+let raiz=path.resolve()
+const { sucess,error } = require(path.join(raiz,'helpers/response'));
+const { userReceived} =require(path.join(raiz,'controllers/controllerUsers'));
 let jsonParser=bodyParser.json();
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 router.param('id',(req,res,next,id)=>{
   console.log('user acepted');
@@ -23,8 +28,21 @@ router.get('/*/:id', (req, res) => {
   res.end()
 })
 router.post('/json',jsonParser,urlencodedParser,(req, res) => {
-  console.log(req.body);
+  let jsonReq=JSON.stringify(req.body);
   //res.setHeader('Content-type','application/json')
-  res.send('se recibio');
+  //res.status(201).send({'message':'recibido correctamtente','data':jsonReq})
+  error(req,res,'datos no recibidos',401)
+})
+
+router.get('/async', async(req,res)=>{
+  const {user,age,pass}=req.body
+  try {
+    await userReceived(user,age,pass);
+    /* let {data}=JOSN.stringify(req.query);
+    const search=await Busqueda.find(data);
+    sucess(req,res,'lista de datos',200,search); */
+  } catch (e) {
+    error(req,res,'error capturado',400,e)
+  }
 })
 module.exports = router;
