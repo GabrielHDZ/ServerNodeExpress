66345b2bbf9494f28339dd6053465ef8d00551d9
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-04-30 00:30:49 -0500

    ruta asministrador de mysql

--- routes/routeMysql.js
+++ routes/routeMysql.js
@@ -2,10 +2,19 @@ let router = require('express').Router();
 let path=require('path');
 let raiz=path.resolve()
 const { sucess,error } = require(path.join(raiz,'helpers/response'));
+const {conexion}=require(path.join(raiz,'/helpers/dbMysqlConnect.js'));
 
 
 router.get('/',(req,res,next)=>{
     sucess(req,res);
+});
+router.post('/',(req,res)=>{
+    conexion(req.body,(error,results,fields)=>{
+        if(error){
+            reject(error);
+        }
+        resolve(results);
+    })
 });
 
 module.exports=router;
