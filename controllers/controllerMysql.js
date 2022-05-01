6728a8883bbd3295ada6b0c55c124eae1fbf36c6
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-04-30 23:02:20 -0500

    route mysql testing conexion user administrator created

--- controllers/controllerMysql.js
+++ controllers/controllerMysql.js
@@ -1,3 +1,11 @@
-const conexion=(data,query,callback)=>{
-    
+let path=require('path');
+let raiz=path.resolve()
+const {conexion}=require(path.join(raiz,'/helpers/dbMysqlConnect.js'));
+
+let existUser=async(req)=>{
+
+    let response=await conexion(req.body);
+    return response;
 }
+
+module.exports={existUser}
--- routes/routeMysql.js
+++ routes/routeMysql.js
@@ -2,19 +2,16 @@ let router = require('express').Router();
 let path=require('path');
 let raiz=path.resolve()
 const { sucess,error } = require(path.join(raiz,'helpers/response'));
-const {conexion}=require(path.join(raiz,'/helpers/dbMysqlConnect.js'));
+const {existUser} = require(path.join(raiz,'/controllers/controllerMysql'));
 
 
 router.get('/',(req,res,next)=>{
     sucess(req,res);
 });
 router.post('/',(req,res)=>{
-    conexion(req.body,(error,results,fields)=>{
-        if(error){
-            reject(error);
-        }
-        resolve(results);
-    })
+    existUser(req)
+    .then(m=>sucess(req,res,m))
+    .catch(e=>error(req,res,e));
 });
 
 module.exports=router;
