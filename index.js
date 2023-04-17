b4fdb493b7a170f04200964bc10ff1436d840ded
GabrielHDZ <gabrielhdz0102@gmail.com>
2023-04-16 23:58:52 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-const express=require('express');
-const path=require('path');
-const logger=require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const sesion=require('express-session');
-const body=require('body-parser');
-const PORT=3000;
+const sesion = require('express-session');
+const body = require('body-parser');
+const PORT = 3000;
 //creamos instancia de express
-const app=express();
+const app = express();
 
 //settings
-app.set('port',process.env.PORT || PORT);
+app.set('port', process.env.PORT || PORT);
 //middlewares
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 
 //static files
 const options = {
     dotfiles: 'ignore',
     etag: true,
-    extensions: ['php','html', 'htm'],
-    fallthrough:true,
-    inmutable:false,
+    extensions: ['php', 'html', 'htm'],
+    fallthrough: true,
+    inmutable: false,
     index: 'index.html',
-    lastModified:true,
+    lastModified: true,
     maxAge: '1d',
     redirect: true,
-    setHeaders: (res, path, stat)=>res.set('x-timestamp', Date.now())
-  }
+    setHeaders: (res, path, stat) => res.set('x-timestamp', Date.now())
+}
 
 //rutas
-app.use('/',express.static(path.join(__dirname,'public'),options))
-app.use('/home',require('./routes/home.js'));
-app.use('/users',require('./routes/routeUsers.js'));
-app.use('/logout',require('./routes/logout.js'));
-app.use('/puppe',require('./routes/routePuppe.js'));
-app.use('/administrador',require('./routes/routeAdministrador.js'));
+app.use('/', express.static(path.join(__dirname, 'public'), options))
+app.use('/home', require('./routes/home.js'));
+app.use('/users', require('./routes/routeUsers.js'));
+app.use('/logout', require('./routes/logout.js'));
+app.use('/puppe', require('./routes/routePuppe.js'));
+app.use('/administrador', require('./routes/routeAdministrador.js'));
 
 //Rutas indefinidas
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.status(404).send('Direccion no existente en este enrutador')
 })
-app.listen(app.get('port'),()=>{
+//server listen
+app.listen(app.get('port'), () => {
     console.log('execute in port 3000')
 })
